f6e3bed8f777868356dc2aeb58f88a4003ba03fc
Your Name <your.email@example.com>
2024-08-02 16:49:45 +0300

    pycode

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,40 +3,23 @@
 Module for safely getting a value from a dictionary.
 """
 
-
 from typing import Any, Mapping, TypeVar, Union
 
 T = TypeVar('T')
 
 
-def safely_get_value(
-        dct: Mapping[Any, Any],
-        key: Any,
-        default: Union[T, None] = None
-) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
-    Safely gets a value from a dictionary.
+    Safely gets a value from a dictionary. If the key exists in the dictionary,
+    returns its value. Otherwise, returns the default value.
 
-    This function takes a dictionary `dct` and a key `key`. It returns the
-    value associated with the key from the dictionary if the key exists in the
-    dictionary. If the key does not exist in the dictionary, it returns the
-    default value provided.
-
-    Args:
-        dct (Mapping[Any, Any]): The dictionary from which to get the value.
-        key (Any): The key to get the value for.
-        default (Union[T, None], optional): The default value to return if the
-            key is not in the dictionary. Defaults to None.
-
-    Returns:
-        Union[Any, T]: The value from the dictionary if the key exists,
-            otherwise the default value.
+    :param dct: Dictionary or mapping from which to get the value
+    :param key: Key to look up in the dictionary
+    :param default: Default value to return if key is not found
+    :return: The value from the dictionary or the default value
     """
-
-    # Check if the key exists in the dictionary
     if key in dct:
-        # Return the value associated with the key
         return dct[key]
     else:
-        # Return the default value
         return default
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,26 +3,21 @@
 Module for zooming in on elements of a tuple.
 """
 
+from typing import List, Tuple, Any
 
-from typing import List, Tuple
 
-
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zoom in on elements of a tuple by a given factor.
 
-    Parameters:
-        lst (Tuple[int, ...]): A tuple of integers.
-        factor (int, optional): The factor by which to zoom in on the elements.
-            Defaults to 2.
-
-    Returns:
-        List[int]: A list with elements repeated according to the factor.
+    :param lst: Tuple of elements
+    :param factor: Factor by which to zoom in on the elements
+    :return: A list with elements repeated according to the factor
     """
-
-    # Create a list by repeating each element in the tuple 'factor' times.
-    zoomed_in = [item for item in lst for _ in range(factor)]
-
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
     return zoomed_in
 
 
